21bd26bfb8daf19e48d39e052a33e3eb29668f13
Gabriel Santos Rodrigues <gabrielsantosrodrigues2000@gmail.com>
2021-12-19 11:33:32 -0300

    Mapeamento de alguns botões e troca do mapeamento do efeito de deslizar com os elementos

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,21 @@
-document.querySelector('#btn-add-1').addEventListener('click', () => {
-  document.querySelector('.container').classList.toggle('change')
-})
+let btnAdicionarTarefa = document.querySelector("#btn-add-1");
+let btnAdicionarCard = document.querySelector("#btn-add-2");
+let cbxBlue = document.querySelector("#checkboxBlue");
+let cbxGreen = document.querySelector("#checkboxGreen");
+let cbxPink = document.querySelector("#checkboxPink");
+let cbxPurple = document.querySelector("#checkboxPurple");
+let cbxOrange = document.querySelector("#checkboxOrange");
+let cbxWhite = document.querySelector("#checkboxWhite");
+
+btnAdicionarTarefa.addEventListener('click', () => {
+  document.querySelector('.container').classList.toggle('change');
+
+});
+
+/* apenas para fins de teste :D
+cbxBlue.addEventListener('click', () => {
+  cbxBlue.src="public/assets/checked.png";
+
+})*/
+
+
