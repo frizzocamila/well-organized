dc8f6d2fb6cee249e181d34c8d7d0bda04ee34cf
Gabriel Santos Rodrigues <gabrielsantosrodrigues2000@gmail.com>
2021-12-21 19:46:28 -0300

    Adição parcial de cards

--- public/js/script.js
+++ public/js/script.js
@@ -7,25 +7,42 @@ let cbxPurple = document.querySelector("#checkboxPurple");
 let cbxOrange = document.querySelector("#checkboxOrange");
 let cbxWhite = document.querySelector("#checkboxWhite");
 let seccaoCores = document.querySelector("#colors"); // fazer array com as cores
-let divContainerTasks = document.getElementsByClassName("container-tasks-todo");
+let divContainerTasks = document.getElementById('container-tasks-todo');
+
+
+function loadCards() {
+  //carregar do localstorage os cards
+  return JSON.parse(localStorage.getItem("stickers") || "[]");
+}
+
+btnAdicionarCard.addEventListener('click', () => {
+  //var cardGenerico = document.getElementByClass('card'); //array
+  
+  divContainerTasks.appendChild(criarCardGenerico());
+  //divContainerTasks.appendChild(cardGenerico);
+});
+
+function criarCardGenerico() {
+    const divElem = document.createElement("div");
+    divElem.classList.add("card");
+    divElem.classList.add("blue-task");
+
+  return divElem;
+}
 
 btnAdicionarTarefa.addEventListener('click', () => {
   document.querySelector('.container').classList.toggle('change');
   /*var btn = document.createElement("BUTTON");
   var t = document.createTextNode("Yeah, man");
   btn.appendChild(t);
-  document.divContainerTasks.appendChild(btn);*/
-
+  document.divContainerTasks.appendChild(btn);*/  
 });
+
 
 function saveCard(/* receber card como parâmetro */) {
   //void function
 }
 
-function loadCards() {
-  //carregar do localstorage os cards
-  //return JSON.parse(localStorage.getItem("stickers") || "[]");
-}
 
 seccaoCores.addEventListener('click', () => {
   //seccaoCores.style.box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.25);
